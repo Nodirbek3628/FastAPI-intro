ed747773058c2decf28721ea851b4fcd019bf67d
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-09-10 11:25:13 +0500

    FastApi.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from random import randint,choice
+import json
 
 app = FastAPI()
 
@@ -7,34 +9,70 @@ app = FastAPI()
 async def root():
     return {"message": "Salom FastAPI !"}
 
+
 @app.get("/fikr")
-async def root():
+async def fikr():
     return {"message": "FastAPI juda zo‘r framework!"}
 
+
 @app.get("/ism ")
-async def root():
-    return {"message": "Mening ismim <..>"}
+async def name():
+    return {"message": "Mening ismim Nodirbek"}
+
 
 @app.get("/yosh")
-async def root():
-    return {"message": "Mening yoshim <..>"}  
+async def age():
+    return {"message": "Mening yoshim 20 da "}  
+
 
 @app.get("/fanlar")
-async def root():
-    return {"message": ""} 
+async def sciences():
+    return ["Matematika", "Fizika", "Ingliz tili"]
 
 @app.get("/talaba")
-async def root():
-    return {"message": ""} 
+async def student():
+    return {
+  "ism": "Ali",
+  "yosh": 20
+}
 
 @app.get("/tasodifiy-son")
-async def root():
-    return {"message": ""} 
+async def random_son():
+    n = randint(1,100)
+
+    return {
+        "son": n
+    }
 
 @app.get("/tasodifiy-fan")
-async def root():
-    return {"message": ""} 
+async def random_science():
+    sciences = ["Matematika", "Fizika", "Ingliz tili", "Tarix", "Biologiya"]
+    fan = choice(sciences)
+
+    return {
+        "fan": fan
+    }
 
 @app.get("/tasodifiy-talaba")
-async def root():
-    return {"message": ""} 
+async def random_student():
+    students  = [{
+        "name": "ALi",
+        "yosh": 20
+    },
+    {
+         "name": "Vali",
+        "yosh": 22
+    },
+    {
+         "name": "John",
+        "yosh": 25
+    },
+    {
+         "name": "Dinara",
+        "yosh": 18
+    }
+    ]
+
+    random_student = choice(students)
+
+    return random_student
